ea8c1665462d2a32dab0015e27bd52daff7efff6
Carlosworld <49792178+Carlosworld@users.noreply.github.com>
2021-01-07 09:59:02 -0600

    Cambios en el navbar

--- js/main.js
+++ js/main.js
@@ -1,71 +1,95 @@
-// Mapa
+// Toggle menu-bar
 
+document.addEventListener('DOMContentLoaded', function() {
+    evenetListener();
+});
+
+function evenetListener() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive(){
+    const navegacion = document.querySelector('.navegacion');
+
+
+    navegacion.classList.toggle('mostrar')
+    // if(navegacion.classList.contains('mostrar')){
+    //     navegacion.classList.remove('mostrar')
+    // }else{
+    //     navegacion.classList.add('mostrar')
+    // }
+}
+
+
+// Mapa
 if (document.getElementById('mapa')) {
 
-    var map = L.map('mapa').setView([17.960890, -102.195890], 17);
+        var map = L.map('mapa').setView([17.960890, -102.195890], 17);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-    L.marker([17.960890, -102.195890]).addTo(map).bindPopup('Albercas del mar').openPopup();
-    // .bindTooltip('GDLWebCamp 2018, Boletos ya disponibles')
-    // .openTooltip();
-}
-(function () {
-    $(function () {
-        // Colorbox
+        L.marker([17.960890, -102.195890]).addTo(map).bindPopup('Albercas del mar').openPopup();
+        // .bindTooltip('GDLWebCamp 2018, Boletos ya disponibles')
+        // .openTooltip();
+    }
+    (function () {
+        $(function () {
+            // Colorbox
 
-        $('body.invitados .navegacion-principal a:contains("Invitados")').addClass('activo');
+            $('body.invitados .navegacion-principal a:contains("Invitados")').addClass('activo');
 
-        $('.alberca-info').colorbox({ inline: true, width: '50%' });
-        $('.boton_newsletter').colorbox({ inline: true, width: '50%' });
-    })
-})();
-
-
-
-
-const url = 'https://fb-api-5835a-default-rtdb.firebaseio.com/anuncios.json'
+            $('.alberca-info').colorbox({ inline: true, width: '50%' });
+            $('.boton_newsletter').colorbox({ inline: true, width: '50%' });
+        })
+    })();
 
 
 
 
-// fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-
-//         let element = document.getElementById('elem')
-
-//         data.forEach(anun => {
-//             element.innerHTML = `
-            
-//             <div class="contenedor-anuncios">
-//             <div class="anuncio">
-//             <img src="${anun.url}" alt="">
-//             <div class="contenido-anuncio">
-//             <h3>${anun.name} </h3>
-//             <p>${anun.descripcion}</p>
-            
-//             </div>
-//             </div>
-//             </div>
-            
-//             `;
-//         });
-//         console.log(element);
+    const url = 'https://fb-api-5835a-default-rtdb.firebaseio.com/anuncios.json'
 
 
-//         console.log(data);
-//     })
-//     .catch(err => console.log(err))
 
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
 
-        let output = ` <div class="contenedor-anuncios">`;
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(data => {
 
-        data.forEach(function(anun)  {
-            output += `
+    //         let element = document.getElementById('elem')
+
+    //         data.forEach(anun => {
+    //             element.innerHTML = `
+
+    //             <div class="contenedor-anuncios">
+    //             <div class="anuncio">
+    //             <img src="${anun.url}" alt="">
+    //             <div class="contenido-anuncio">
+    //             <h3>${anun.name} </h3>
+    //             <p>${anun.descripcion}</p>
+
+    //             </div>
+    //             </div>
+    //             </div>
+
+    //             `;
+    //         });
+    //         console.log(element);
+
+
+    //         console.log(data);
+    //     })
+    //     .catch(err => console.log(err))
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+
+            let output = ` <div class="contenedor-anuncios">`;
+
+            data.forEach(function (anun) {
+                output += `
             
             <div class="anuncio">
             <img src="${anun.url}" alt="">
@@ -76,14 +100,13 @@ fetch(url)
             </div>
             </div>
             `;
-        });
-        document.getElementById('elem').innerHTML=output;
-        console.log(element);
+            });
+            document.getElementById('elem').innerHTML = output;
+            // console.log(element);
 
 
-        console.log(data);
-    })
-    .catch(err => console.log(err))
+            console.log(data);
+        })
+    // .catch(err => console.log(err))
 
 
-  
