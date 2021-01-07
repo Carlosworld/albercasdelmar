3fc4106f927a6ff02a92ea6f15cb00b4e4429bd9
Carlosworld <49792178+Carlosworld@users.noreply.github.com>
2021-01-07 15:50:43 -0600

    actualizacion

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Toggle menu-bar
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     evenetListener();
 });
 
@@ -10,7 +10,7 @@ function evenetListener() {
     mobileMenu.addEventListener('click', navegacionResponsive);
 }
 
-function navegacionResponsive(){
+function navegacionResponsive() {
     const navegacion = document.querySelector('.navegacion');
 
 
@@ -26,87 +26,53 @@ function navegacionResponsive(){
 // Mapa
 if (document.getElementById('mapa')) {
 
-        var map = L.map('mapa').setView([17.960890, -102.195890], 17);
+    var map = L.map('mapa').setView([17.960890, -102.195890], 17);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-        L.marker([17.960890, -102.195890]).addTo(map).bindPopup('Albercas del mar').openPopup();
-        // .bindTooltip('GDLWebCamp 2018, Boletos ya disponibles')
-        // .openTooltip();
-    }
-    (function () {
-        $(function () {
-            // Colorbox
+    L.marker([17.960890, -102.195890]).addTo(map).bindPopup('Albercas del mar').openPopup();
+    // .bindTooltip('GDLWebCamp 2018, Boletos ya disponibles')
+    // .openTooltip();
+}
+(function () {
+    $(function () {
+        // Colorbox
 
-            $('body.invitados .navegacion-principal a:contains("Invitados")').addClass('activo');
+        $('body.invitados .navegacion-principal a:contains("Invitados")').addClass('activo');
 
-            $('.alberca-info').colorbox({ inline: true, width: '50%' });
-            $('.boton_newsletter').colorbox({ inline: true, width: '50%' });
-        })
-    })();
-
-
-
-
-    const url = 'https://fb-api-5835a-default-rtdb.firebaseio.com/anuncios.json'
+        $('.alberca-info').colorbox({ inline: true, width: '50%' });
+        $('.boton_newsletter').colorbox({ inline: true, width: '50%' });
+    })
+})();
 
 
 
 
-    // fetch(url)
-    //     .then(response => response.json())
-    //     .then(data => {
+const url = 'https://fb-api-5835a-default-rtdb.firebaseio.com/anuncio.json';
 
-    //         let element = document.getElementById('elem')
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
 
-    //         data.forEach(anun => {
-    //             element.innerHTML = `
+        let output = ` <div class="contenedor-anuncios">`;
 
-    //             <div class="contenedor-anuncios">
-    //             <div class="anuncio">
-    //             <img src="${anun.url}" alt="">
-    //             <div class="contenido-anuncio">
-    //             <h3>${anun.name} </h3>
-    //             <p>${anun.descripcion}</p>
-
-    //             </div>
-    //             </div>
-    //             </div>
-
-    //             `;
-    //         });
-    //         console.log(element);
-
-
-    //         console.log(data);
-    //     })
-    //     .catch(err => console.log(err))
-
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-
-            let output = ` <div class="contenedor-anuncios">`;
-
-            data.forEach(function (anun) {
-                output += `
-            
+        data.forEach(function (anun) {
+            output += `
             <div class="anuncio">
             <img src="${anun.url}" alt="">
             <div class="contenido-anuncio">
             <h3>${anun.name} </h3>
             <p>${anun.descripcion}</p>
-            
             </div>
             </div>
             `;
-            });
-            document.getElementById('elem').innerHTML = output;
-            // console.log(element);
+        });
+        document.getElementById('elem').innerHTML = output;
+        // console.log(element);
 
 
-            console.log(data);
-        })
+        console.log(Object.keys(data));
+    })
     // .catch(err => console.log(err))
 
 
