3a0f7d72e8e2aeb8fb4602abd78c535c1a58e866
Carlosworld <49792178+Carlosworld@users.noreply.github.com>
2021-01-20 14:43:37 -0600

    Imagenes y datos agregados desdel administrador

--- js/main.js
+++ js/main.js
@@ -34,45 +34,94 @@ if (document.getElementById('mapa')) {
     // .bindTooltip('GDLWebCamp 2018, Boletos ya disponibles')
     // .openTooltip();
 }
-(function () {
-    $(function () {
-        // Colorbox
+// (function () {
+//     $(function () {
+//         // Colorbox
 
-        $('body.invitados .navegacion-principal a:contains("Invitados")').addClass('activo');
+//         $('body.invitados .navegacion-principal a:contains("Invitados")').addClass('activo');
 
-        $('.alberca-info').colorbox({ inline: true, width: '50%' });
-        $('.boton_newsletter').colorbox({ inline: true, width: '50%' });
-    })
-})();
+//         $('.alberca-info').colorbox({ inline: true, width: '50%' });
+//         $('.boton_newsletter').colorbox({ inline: true, width: '50%' });
+//     })
+// })();
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCfwWEYfalbck6zdiwdfwIsPQYZ_hJdXd8",
+    authDomain: "fb-api-5835a.firebaseapp.com",
+    databaseURL: "https://fb-api-5835a-default-rtdb.firebaseio.com",
+    projectId: "fb-api-5835a",
+    storageBucket: "fb-api-5835a.appspot.com",
+    messagingSenderId: "225448914734",
+    appId: "1:225448914734:web:9bf9354378ab882add14bf",
+    measurementId: "G-8JZJJDZM18"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
+const db = firebase.firestore()
 
+db.collection('anuncio-albercas-del-mar').get()
+    .then(snapshot => {
+        let data = [];
+        snapshot.forEach(doc => {
+            data.push(
+                Object.assign(
+                    {
+                        id: doc.id
+                    },
+                    doc.data()
+                )
+            );
+        });
+        console.log(data)
 
-const url = 'https://fb-api-5835a-default-rtdb.firebaseio.com/anuncio.json';
-
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
 
         let output = ` <div class="contenedor-anuncios">`;
 
         data.forEach(function (anun) {
             output += `
-            <div class="anuncio">
-            <img src="${anun.url}" alt="">
-            <div class="contenido-anuncio">
-            <h3>${anun.name} </h3>
-            <p>${anun.descripcion}</p>
-            </div>
-            </div>
-            `;
+                <div class="anuncio">
+                <img src="${anun.url}" alt="">
+                <div class="contenido-anuncio">
+                <h3>${anun.name} </h3>
+                <p>${anun.descripcion}</p>
+                </div>
+                </div>
+                `;
         });
-        document.getElementById('elem').innerHTML = output;
+        document.getElementById('elem').innerHTML = output
         // console.log(element);
-
-
-        console.log(Object.keys(data));
     })
-    // .catch(err => console.log(err))
+    .catch(err => console.log(err));
 
 
+
+// const url = 'https://fb-api-5835a-default-rtdb.firebaseio.com/anuncio.json';
+
+// fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+
+//         let output = ` <div class="contenedor-anuncios">`;
+
+//         data.forEach(function (anun) {
+//             output += `
+//             <div class="anuncio">
+//             <img src="${anun.url}" alt="">
+//             <div class="contenido-anuncio">
+//             <h3>${anun.name} </h3>
+//             <p>${anun.descripcion}</p>
+//             </div>
+//             </div>
+//             `;
+//         });
+//         document.getElementById('elem').innerHTML = output;
+//         // console.log(element);
+
+
+//         console.log(Object.keys(data));
+//     })
+//     // .catch(err => console.log(err))
